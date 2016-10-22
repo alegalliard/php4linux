d5dd1d424130b2b26418b09c3615fbd56e39bc41
alegalliard <alisson.machado@4linux.com.br>
2016-10-22 10:38:18 -0200

    visibilidade

---
+++ aula2/encapsulamento.php
@@ -0,0 +1,6 @@
+<?php
+////impedir que os atributos do objeto sejam acessadas e alteradas indevidamente
+//public, protected e private
+//public - toda instancia permite que seja visualizado
+//protected - a propria classe tem acesso a ele e seus filhos. O objeto instanciado nao tem acesso direto a ele
+//private - apenas a propria classe. Nem quem faz heranca consegue acessar
---
+++ aula2/visibilidade.php
@@ -0,0 +1,36 @@
+<?php
+
+class Conta {
+	protected $titular;
+	protected $saldo;
+	
+	public function depositar($valor) {
+		$this->saldo += $valor;
+	}
+	public function verSaldo() {
+		echo "<hr> Saldo atual: {$this->saldo}</hr>";
+	}
+	
+	public function verTitular() {
+		echo "<hr> Titular: {$this->titular}</hr>";
+		
+	}
+}
+
+class Poupanca extends Conta {
+	
+	public function nomearTitular($nome){
+		$this->titular = $nome;
+		
+	}
+	public function mostrarTipoConta(){
+		echo "<hr> Sou uma conta poupanca</hr>";
+	}
+	
+}
+
+$cp = new Poupanca();
+$cp->nomearTitular("Maria");
+$cp->depositar(300);
+$cp->verSaldo();
+$cp->verTitular();
